33be6a53c64b25c3f78f536166d9e8506bb7868b
breakthrough23 <108655207+breakthrough23@users.noreply.github.com>
2024-01-02 19:36:20 +0100

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any content <br />
+          Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. it's 100% Javascript
+          and fully customizable
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
